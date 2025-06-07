ccbcdcee75d58aa13519966c69be16111cc67daa
azizhmd74 <abdelaziz.hammoudi@univ-constantine2.dz>
2025-06-07 20:30:14 +0100

    trying to change on the first commit adding the projects and project list

---
+++ src/Assets/data/Front-data.js
@@ -0,0 +1,75 @@
+import img1 from "../../Assets/projectsImages/ui-venti.jpeg";
+import img2 from "../../Assets/projectsImages/maktabat-atawassol.png";
+import img3 from "../../Assets/projectsImages/portfolio.jpeg";
+// import img4 from "../../Assets/projectsImages/notekeeper.jpeg";
+// import img5 from "../../Assets/projectsImages/emojipidea.jpeg";
+import img6 from "../../Assets/projectsImages/venti-Backend.jpeg";
+// import img7 from "../../Assets/projectsImages/ecommerce.jpeg";
+
+
+export const projectsData = [
+    {
+        id: 1,
+        title: "Venti : Social E-commerce",
+        desc: "A dynamic Social e-commerce platform specializing in second-hand clothing, promoting sustainable fashion choices while providing a social experience.",
+        img: img1,
+        link: "https://venti-algeria.netlify.app/",
+        gitLink: "https://github.com/Abdelazizhammoudi/Venti-Frontend-ReactJs",
+        tech: "Vanilla css",
+      },
+
+{
+    id: 2,
+    title: "E-commerce",
+    desc: "A dynamic e-commerce platform built from scratch for a client who is selling his products online using React.js and django rest framework deployed on a vps server using NGINX and Gunicorn.",
+    img: img2,
+    link: "https://maktabat-atawassol.com/",
+    gitLink: "https://github.com/Abdelazizhammoudi/React_ecommerce_Frontend/tree/master",
+    tech: "Vanilla css , React.js , Django Rest Framework , NGINX , Gunicorn",
+},
+{
+    id: 3,
+    title: "Portfolio",
+    desc: "My React.js portfolio is an interactive online resume showcasing my web development skills and projects.",
+    img: img3,
+    link: "https://maktabat-atawassol.com/",// ill change it later
+    gitLink: "https://github.com/Abdelazizhammoudi/Portfolio",
+    tech: "React.js , Tailwind css",
+},
+// {
+//     id: 4,
+//     title: "Notekeeper",
+//     desc: "A small Notekeeper react project",
+//     img: img4,
+//     link: "https://maktabat-atawassol.com/",// ill change it later
+//     gitLink: "https://github.com/Abdelazizhammoudi/NoteKeeperReact",
+//     tech: "React.js , vanilla css",
+// },
+// {
+//     id: 5,
+//     title: "Emojipidea",
+//     desc: "A small Emojipidea react project",
+//     img: img5,
+//     link: "https://maktabat-atawassol.com/",// ill change it later
+//     gitLink: "https://github.com/Abdelazizhammoudi/Emojipidea-react",
+//     tech: "React.js , vanillacss",
+// },
+{
+    id: 6,
+    title: "venti social ecommerce backend",
+    desc: "The backend for the venti social ecommerce project using django rest framework that i concepted and built from scratch",
+    img: img6,
+    link: "https://maktabat-atawassol.com/",// ill change it later
+    gitLink: "https://github.com/Abdelazizhammoudi/venti",
+    tech: "Django Rest Framework",
+},
+// {
+//     id: 7,
+//     title: "venti social ecommerce backend",
+//     desc: "The backend for the full stack ecommerce project using django rest framework for the client who is selling his products online",
+//     img: img7,
+//     link: "https://maktabat-atawassol.com/",
+//     gitLink: "https://github.com/Abdelazizhammoudi/DRF_Ecommerce_backend",
+//     tech: "Django Rest Framework",
+// },
+];
---
+++ src/ProjectList/ProjectList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ProjectCards from "../Projects/ProjectCards";
+import "./projectList.css";
+import { projectsData } from "../../Assets/data/Front-data";
+
+const ProjectList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">My projects</h1>
+        <p className="pl-desc">
+          Each project is a unique piece of development 🧩 Click on the picture
+          to go to the website (Live Demo).
+        </p>22
+      </div>
+      <div className="pl-list">
+        {projectsData.map((item) => {
+          return (
+            <ProjectCards
+              key={item.id}
+              title={item.title}
+              desc={item.desc}
+              img={item.img}
+              link={item.link}
+              gitLink={item.gitLink}
+              tech={item.tech}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
